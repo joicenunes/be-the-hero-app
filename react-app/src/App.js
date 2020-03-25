0f4006505f1868b71cca9d1fddd106fae2c8961b
joicenunes <anunes.joice@gmail.com>
2020-03-25 15:29:36 -0300

    initial config frontend

---
+++ react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+/* JSX = JavaScript XML = HTML no JS */
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
